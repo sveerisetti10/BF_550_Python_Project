f84c9b3ca6da847059ce14a0f63adc43d8eb75b4
Sri Veerisetti <98369110+sveerisetti10@users.noreply.github.com>
2022-12-08 18:55:05 -0600

    Add files via upload

--- ellipse_weighted.R
+++ ellipse_weighted.R
@@ -1,17 +1,21 @@
-#Ellipse Weighted
 
+#Ellipse Weighted
 library(dplyr)
 library(tidyverse)
 #install.packages('ggforce')
 library(ggforce)
 #install.packages("plotly")
 library(plotly)
+library(ggfortify)
 
-pca_data <- read_csv('/Users/sveerisetti/Desktop/hello.csv')
+
+pca_data <- read_csv('/Users/sveerisetti/Desktop/Import/pca_data_weighted.csv')
 pca_data <- pca_data[, -1]
+pca_data$PC1 <- pca_data$PC1 * c(-1)
 
 Variable <- pca_data$Variable 
 Group <- pca_data$Group
+pca_data <- pca_data[,-c(3,4)]
 
 PCA <- pca_data %>%
   ggplot(aes(x = PC1, y = PC2))+
@@ -19,7 +23,14 @@ PCA <- pca_data %>%
   geom_mark_ellipse(aes(fill = Variable,label = Variable),
                     expand = unit(0.5,"mm"),
                     label.buffer = unit(-5, 'mm'))+
+  xlab('PC1 -Percent variation explained 57.343%')+
+  ylab('PC2 -Percent variation explained 10.369%')+
   xlim(-0.5, 0.5)+
   ylim(-0.3, 0.4)
 
 PCA
+
+
+
+
+
