8c044f77d3e6d52dd4f645b9bc0ad8d4a6b3b8bc
Sri Veerisetti <98369110+sveerisetti10@users.noreply.github.com>
2022-12-11 15:10:56 -0600

    Add files via upload

--- ellipse_weighted.R
+++ ellipse_weighted.R
@@ -8,15 +8,23 @@ library(ggforce)
 library(plotly)
 library(ggfortify)
 
-
+#' Here we want to load in our dataframe that contains the PC coordinates, groups, 
+#' and Variable
 pca_data <- read_csv('/Users/sveerisetti/Desktop/Import/pca_data_weighted.csv')
 pca_data <- pca_data[, -1]
 pca_data$PC1 <- pca_data$PC1 * c(-1)
+
+#' We want to save the Variable and Group information to separate variables
+#' so that we use them later when plotting
 
 Variable <- pca_data$Variable 
 Group <- pca_data$Group
 pca_data <- pca_data[,-c(3,4)]
 
+
+#' Finally, we can use ggplot2 in order to produce the PCoA plot. 
+#' The geom_point() function can be used to develop the scatter plot 
+#' The geom_mark_ellipse() function is used to create our ellipses
 PCA <- pca_data %>%
   ggplot(aes(x = PC1, y = PC2))+
   geom_point(aes(color = Group))+
@@ -26,8 +34,10 @@ PCA <- pca_data %>%
   xlab('PC1 -Percent variation explained 57.343%')+
   ylab('PC2 -Percent variation explained 10.369%')+
   xlim(-0.5, 0.5)+
-  ylim(-0.3, 0.4)
+  ylim(-0.3, 0.4) +
+  ggtitle('PCoA- Weighted UniFrac')
 
+# Display the Plot
 PCA
 
 
